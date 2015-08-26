5d08457d9c122c14edd5a54bc1131fa1af327711
Jack Mallers <jimmymowschess@gmail.com>
2015-08-26 15:22:47 -0500

    findDocs added and tests passing

--- axios-index.js
+++ axios-index.js
@@ -5,6 +5,11 @@ var OpenpublishState = function(baseOptions) {
   var network = baseOptions.network;
   var baseUrl = baseOptions.baseUrl || network == "testnet" ? "https://bsync-test.blockai.com" : "https://bsync.blockai.com";
 
+  var processOpenpublishDoc = function(doc) {
+    doc.txid = doc.txid || doc.txout_tx_hash;
+    return doc;
+  };
+
   var findTips = function(options) {
     console.log(options.sha1);
     return axios.get(baseUrl + "/opendocs/sha1/" + options.sha1 + "/tips")
@@ -14,11 +19,13 @@ var OpenpublishState = function(baseOptions) {
           totalTipAmount += res.data[i].amount;
         }
 
-        return {
+        var data =  {
           tips: res.data,
           totalTipAmount: totalTipAmount,
           tipCount: res.data.length
         };
+
+        return { data: data };
       })
       .catch(function(err) {
         return { err: err };
@@ -28,16 +35,43 @@ var OpenpublishState = function(baseOptions) {
   var findAllTips = function(options) {
     return axios.get(baseUrl + "/opentips")
       .then(function(res) {
-        return res.data;
+        return { data: res.data };
       })
       .catch(function(err) {
         return { err: err };
       });
   };
 
+  var findDoc = function(options) {
+    return axios.get(baseUrl + "/opendocs/sha1/" + options.sha1)
+      .then(function(res) {
+        var openpublishDoc = res.data;
+        if (!openpublishDoc) {
+          return { err: true };
+        }
+        processOpenpublishDoc(openpublishDoc);
+        if (options.includeTips) {
+          findTips({sha1:sha1})
+            .then(function(res) {
+              openpublishDoc.totalTipAmount = res.totalTipAmount;
+              openpublishDoc.tipCount = res.tipCount;
+              openpublishDoc.tips = res.tips;
+            })
+            .catch(function(err) {
+              return { err: err }
+            });
+            return { data: openpublishDoc };
+        }
+        else {
+          return { data: openpublishDoc };
+        }
+    });
+  };
+
   return {
     findTips: findTips,
-    findAllTips: findAllTips
+    findAllTips: findAllTips,
+    findDoc: findDoc
   }
 
 };
--- axios-test.js
+++ axios-test.js
@@ -7,9 +7,9 @@ var openpublishState = require('./axios-index')({
 test('should find the tips for a document with a sha1 of 2dd0b83677ac2271daab79782f0b9dcb4038d659', function(t) {
    openpublishState.findTips({ sha1: "2dd0b83677ac2271daab79782f0b9dcb4038d659" })
       .then(function(res) {
-         t.ok(res.tipCount > 0, 'tipCount');
-         t.ok(res.totalTipAmount > 0, 'totalTipAmount');
-         t.ok(res.tips.length > 0, 'tips');
+         t.ok(res.data.tipCount > 0, 'tipCount');
+         t.ok(res.data.totalTipAmount > 0, 'totalTipAmount');
+         t.ok(res.data.tips.length > 0, 'tips');
          t.end();
       });
 });
@@ -19,7 +19,7 @@ test('should not find the tips for a document with a sha1 of XXX', function(t) {
       .then(function(res) {
          // this should actually return an err and tipInfo == false, but we need an api change on bsync
          t.ok(!res.err, 'err is false');
-         t.ok(res.tips.length === 0, 'tipInfo.tips is empty');
+         t.ok(res.data.tips.length === 0, 'tipInfo.tips is empty');
          t.end();
       });
 });
@@ -29,9 +29,23 @@ test('should find all open tips', function (t) {
   openpublishState.findAllTips({})
    .then(function(res) {
       t.ok(!res.err, 'err is false');
-      t.ok(res.length > 0, "at least found some open tips");
-      t.ok(res[0].tx_out_hash !== null, "txid of tip is not null");
-      t.ok(res[0].sha1 !== null, "sha1 of tip is not null");
+      t.ok(res.data.length > 0, "at least found some open tips");
+      t.ok(res.data[0].tx_out_hash !== null, "txid of tip is not null");
+      t.ok(res.data[0].sha1 !== null, "sha1 of tip is not null");
       t.end();
    });
+});
+
+// findDoc
+test('should find a document with a sha1 of 2dd0b83677ac2271daab79782f0b9dcb4038d659', function(t) {
+   openpublishState.findDoc({sha1: "2dd0b83677ac2271daab79782f0b9dcb4038d659"})
+      .then(function(res) {
+         t.equal(res.data.output.tx_hash, '2587d9a8662afb37fc32bfdb4914c2d08a134fd709cb7a84db08a22ca578dedf', 'txid');
+         t.equal(res.data.sourceAddresses[0], 'msLoJikUfxbc2U5UhRSjc2svusBSqMdqxZ', 'sourceAddresses');
+         t.equal(res.data.name, 'ega_art_detail.gif', 'name');
+         t.equal(res.data.btih, '9028e4200b976662d11159c2b16a6c12ef83d967', 'btih');
+         t.equal(res.data.sha1, '2dd0b83677ac2271daab79782f0b9dcb4038d659', 'sha1');
+         t.ok(!res.data.tips, 'no tips');
+         t.end();
+      });
 });
